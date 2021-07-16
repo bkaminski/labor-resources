f2575da4e92ac1ec88ef6e200e2301790b9294f3
Ben Kaminski <ben@benkaminski.com>
2021-07-16 14:19:01 -0400

    added team members

--- parts/page-the-team.php
+++ parts/page-the-team.php
@@ -4,26 +4,33 @@
 			<div class="col-md-3 pt-5">
 				<h2 class="blue-text fw-bold h1">The Team</h2>
 			</div>
-			<div class="col-md-7 pt-5 pb-5">
+			<div class="col-md-9 pt-5 pb-5">
 				<div class="row g-0">
-					<div class="col-md-4 img-hover" style="background: #e96572;">
-						<img src="<?php echo the_field('team1');?>" class="img-fluid pichover" alt="<?php echo the_field('team_1_img_alt'); ?>" />
-							
+					<div class="col-md-3" style="background: #fff;">
+						<figure class="figure text-center">
+							<img src="<?php echo the_field('team1');?>" class="img-fluid figure-img" alt="<?php echo the_field('team_1_img_alt'); ?>" />
+							<figcaption class="figure-caption"><?php echo the_field('emp_title1'); ?></figcaption>
+						</figure>
 					</div>
-					<div class="col-md-4 img-hover" style="background: #e96572;">
-						<img src="<?php echo the_field('team2');?>" class="img-fluid pichover" alt="<?php echo the_field('team_2_img_alt'); ?>" />
+					<div class="col-md-3 img-hover" style="background: #fff;">
+						<figure class="figure text-center">
+						<img src="<?php echo the_field('team2');?>" class="img-fluid" alt="<?php echo the_field('team_2_img_alt'); ?>" />
+						<figcaption class="figure-caption"><?php echo the_field('emp_title2'); ?></figcaption>
 					</div>
-					<div class="col-md-4 img-hover" style="background: #e96572;">
-						<img src="<?php echo the_field('team3');?>" class="img-fluid pichover" alt="<?php echo the_field('team_3_img_alt'); ?>" />
+					<div class="col-md-3" style="background: #fff;">
+						<figure class="figure text-center">
+							<img src="<?php echo the_field('team3');?>" class="img-fluid" alt="<?php echo the_field('team_3_img_alt'); ?>" />
+							<figcaption class="figure-caption"><?php echo the_field('emp_title3'); ?></figcaption>
+						</figure>
 					</div>
-				</div>
-				<div class="row g-0">
-					<div class="col-md-4 img-hover" style="background: #e96572;">
-						<img src="<?php echo the_field('team4');?>" class="img-fluid pichover" alt="<?php echo the_field('team_4_img_alt'); ?>" />
+					<div class="col-md-3" style="background: #fff;">
+						<figure class="figure text-center">
+						<img src="<?php echo the_field('team4');?>" class="img-fluid" alt="<?php echo the_field('team_4_img_alt'); ?>" />
+						<figcaption class="figure-caption"><?php echo the_field('emp_title4'); ?></figcaption>
 					</div>
+					
 				</div>
 			</div>
-			<div class="col-md-2"></div>
 		</div>
 	</div>
 </aside>
